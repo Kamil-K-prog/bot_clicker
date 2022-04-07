bc278a7b06a5b41e5c8df4f0df15f17c6384aeb1
Камиль <kamil200571@gmail.com>
2022-04-07 20:40:02 +0500

    Переделаны таблицы БД. Переделана проверка имени нового пользователя. Перед запуском удалить старую версию БД!!!

--- data/modificators.py
+++ data/modificators.py
@@ -5,7 +5,7 @@ from sqlalchemy import orm
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(SqlAlchemyBase):
+class Modificators(SqlAlchemyBase):
     __tablename__ = 'modificators'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
@@ -13,4 +13,4 @@ class User(SqlAlchemyBase):
     multiplier = sqlalchemy.Column(sqlalchemy.Integer, default=1)
     duration = sqlalchemy.Column(sqlalchemy.Integer, default=1)
 
-    #jobs = orm.relation("Jobs", back_populates="user")
+    user = orm.relation("User", back_populates='modificators')
