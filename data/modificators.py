cde5fbdfa7a6e83b65433af3b5b09e21d9252cbc
Qurclinc <pastghost2@mail.ru>
2022-04-12 06:50:16 +0500

    слегка доработан интерфейс; реализованы модификаторы и их взаимодействие с пользователем, в целом. возможны ошибки, требуются тестирования.

--- data/modificators.py
+++ data/modificators.py
@@ -11,6 +11,6 @@ class Modificators(SqlAlchemyBase):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.Text, unique=True)
     multiplier = sqlalchemy.Column(sqlalchemy.Integer, default=1)
-    duration = sqlalchemy.Column(sqlalchemy.Integer, default=1)
+    price = sqlalchemy.Column(sqlalchemy.Integer, default=1)
 
     user = orm.relation("User", back_populates='modificators')
